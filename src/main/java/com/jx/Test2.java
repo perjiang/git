bc8b9648f956aca2470ff304c440c874d688ba96
jiangx <jiangx@qianlong2.net>
2022-07-14 15:55:07 +0800

    update Test2.java

--- src/main/java/com/jx/Test2.java
+++ src/main/java/com/jx/Test2.java
@@ -9,5 +9,7 @@ package com.jx;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("test2");
+        System.out.println("test3");
+        System.out.println("test3");
     }
 }
