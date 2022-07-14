3248138f7924b18bfe9045aa5b4abff9ebf67fd4
jiangx <jiangx@qianlong2.net>
2022-07-14 15:53:26 +0800

    add Test3.java

---
+++ src/main/java/com/jx/Test3.java
@@ -0,0 +1,13 @@
+package com.jx;
+
+/**
+ * @ClassName Test1
+ * @Description
+ * @Author
+ * @Date 2022/7/14 15:30
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("test3");
+    }
+}
