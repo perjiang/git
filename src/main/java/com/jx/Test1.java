9e36b05af51d64cda0b4917a49243c30f580e62d
jiangx <jiangx@qianlong2.net>
2022-07-14 15:30:43 +0800

    1

---
+++ src/main/java/com/jx/Test1.java
@@ -0,0 +1,13 @@
+package com.jx;
+
+/**
+ * @ClassName Test1
+ * @Description TODO
+ * @Author
+ * @Date 2022/7/14 15:30
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
